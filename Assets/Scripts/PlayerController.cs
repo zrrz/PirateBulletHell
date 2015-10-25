58d2cad2208998cf2106ef6537abdaac28734528
Zack Rock <zacharyrock@hotmail.com>
2015-10-24 20:43:54 -0700

    Waves added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+	[SerializeField]
+	private float speed = 1.0f;
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButton("Fire1")) {
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			if(hit.collider != null) {
+				Vector2 dir = hit.point - new Vector2(transform.position.x, transform.position.y);
+
+				transform.position += (transform.up * speed);
+				transform.rotation = Quaternion.LookRotation(Vector3.forward, dir);
+				print(transform.up);
+			}
+		}
 	}
 }
